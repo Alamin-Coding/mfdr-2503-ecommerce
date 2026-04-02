943ab95a4dad91a863beb6da8b0fd9661fd6b6dd
Alamin-Coding <md.alamin.coding@gmail.com>
2026-04-02 18:17:34 +0600

    blog page add

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div>Blog
+
+        <BlogCart/>
+    </div>
+  )
+}
+
+const BlogCart = ()=> {
+    return (
+        <p>Blog Card</p>
+    )
+}
+
+
+export default Blog
